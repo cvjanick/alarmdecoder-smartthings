57d6b8e1c352200109368c50d6ecb653582ac288
cvjanick <cvjanick@gmail.com>
2017-11-19 19:10:45 -0500

    Read/View only child device for physical zones
    Modified/refactored version of virtual_contact_sensor.groovy with attributes to show zone number and description retrieved from AlarmDecoder WebApp.

---
+++ VirtualZoneSensor.groovy
@@ -0,0 +1,44 @@
+/* Virtual Contact Sensor for alarm panel zones */
+// 
+// cjanick updates: added attributes for zoneID, zoneName, ZoneDescription
+
+metadata {
+    definition (name: "VirtualZoneSensor", namespace: "alarmdecoder", author: "scott@nutech.com") {
+        capability "Contact Sensor"
+        capability "Refresh"
+    }
+    
+    attribute "zoneID",          "number"
+    attribute "zoneName",        "string"
+    attribute "zoneDescription", "string"
+    
+    command "refresh"
+
+    // tile definitions 
+    tiles (scale: 2)  {
+        standardTile("sensor", "device.contact", width: 2, height: 2, canChangeIcon: true) {
+            state "closed", label: '${name}', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
+            state "open", label: '${name}', icon: "st.contact.contact.open", backgroundColor: "#ff0000"
+        }
+
+        standardTile("zone_id_tile", "device.zoneID", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", label: 'Zone ${currentValue}'
+        }
+        standardTile("zone_name_tile", "device.zoneName", inactiveLabel: false, decoration: "flat", width: 6, height: 2) {
+            state "default", label: '${currentValue}'
+        }
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", label:'Refresh', action:"device.refresh", icon: "st.secondary.refresh-icon"
+        }
+
+        main "sensor"
+        details(["sensor", "zone_id_tile",   "refresh",
+                 "zone_name_tile"])
+    }
+}
+
+/** commands **/
+def refresh() {
+   parent.refresh()
+   }
+   
